27f3ad7245cfb803887c0e76058749fa9842f6c3
Alexander Kharichev <myx@myx.ru>
2022-04-26 17:09:13 +0300

    * pkg bootstrap

--- sh-scripts/install-myx.common-freebsd.sh
+++ sh-scripts/install-myx.common-freebsd.sh
@@ -12,6 +12,8 @@
 echo "myx.common FreeBSD Installer started..." >&2
 test `id -u` != 0 && echo 'ERROR: Must be root!' >&2 && exit 1
 
+pkg bootstrap -y ; [ ! -z "$( pkg info | grep ca_root )" ] || pkg install -y ca_root_nss
+
 fetch https://github.com/myx/os-myx.common/archive/master.tar.gz -o - | \
 		tar zxvf - -C "/usr/local/" --include "*/host/tarball/*" --strip-components 3
 		
